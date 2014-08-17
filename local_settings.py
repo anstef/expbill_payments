356e6dce105b29221f3ba56f40d4110e81cf975a
Andrey Ovchinnikov <aovchinnikov@rtcsoftware.ru>
2014-08-17 15:43:57 +0400

    Add localsettings

---
+++ local_settings.py
@@ -0,0 +1,15 @@
+#sqlite path
+db_name = '/tmp/payments.db'
+
+#qiwi
+qiwi_login = 'login'
+qiwi_pass = 'pass'
+
+#billing
+billing_payment_url = 'http://127.0.0.1/payments.simpleterminal/simpleterminal/payment/'
+billing_secret = '123456'
+
+#email
+email_from = 'robot@yoursite.com'
+email_from_pass = 'q123'
+email_to = 'admin@yoursite.com'
